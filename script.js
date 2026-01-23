19e1f0247d9ff326322f914bae238a6c3b6433f1
SOARES <142699575+GHOSTGOSTOSO@users.noreply.github.com>
2026-01-23 09:44:06 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -1,18 +1,6 @@
-const btn = document.querySelector(".btn-trabalhos");
-
-btn.addEventListener("mousemove", (e) => {
-  const rect = btn.getBoundingClientRect();
-  const x = e.clientX - rect.left;
-  const y = e.clientY - rect.top;
-
-  btn.style.setProperty("--x", `${x}px`);
-  btn.style.setProperty("--y", `${y}px`);
-});
+const btn = document.getElementById("btnTrabalhos");
+const section = document.getElementById("trabalhos");
 
 btn.addEventListener("click", () => {
-  btn.classList.add("clicked");
-
-  setTimeout(() => {
-    btn.classList.remove("clicked");
-  }, 300);
+  section.scrollIntoView({ behavior: "smooth" });
 });
